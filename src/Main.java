1c6c072f30658b1b9abe36b8ad9c8a5b4f04e4af
Itonda <anthellic@gmail.com>
2025-02-02 16:42:18 -0700

    Add Main.java

---
+++ src/Main.java
@@ -0,0 +1,46 @@
+// Need this to prompt input. //
+import java.util.Scanner;
+
+// Initializes main program. //
+public class Main {
+    public static void main(String[] args) {
+
+        // Initializing variables for calculation. //
+        double income;
+        double taxHold;
+
+        // Initialize scanner. //
+        Scanner inc = new Scanner(System.in);
+
+        // Prompt user input and declare income as user input. //
+        System.out.print("Enter income: ");
+        income = inc.nextDouble();
+
+        /* If/Else ladder to determine tax withheld and print result.
+           10% for less than 500, 15% for 500 - 1500, 20% for 1500 - 2500,
+           and 30% for more than 2500. */
+        if (income < 500) {
+            taxHold = income * 0.1;
+            System.out.println("Entered weekly income: " + income);
+            System.out.printf("Tax Withheld: %.2f ", taxHold);
+        }
+
+        else if (income < 1500) {
+            taxHold = income * 0.15;
+            System.out.println("Entered weekly income: " + income);
+            System.out.printf("Tax Withheld: %.2f ", taxHold);
+        }
+
+        else if (income < 2500) {
+            taxHold = income * 0.2;
+            System.out.println("Entered weekly income: " + income);
+            System.out.printf("Tax Withheld: %.2f ", taxHold);
+        }
+
+        else {
+            taxHold = income * 0.3;
+            System.out.println("Entered weekly income: " + income);
+            System.out.printf("Tax Withheld: %.2f ", taxHold);
+        }
+    }
+}
